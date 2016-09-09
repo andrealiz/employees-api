17f635583f3f33195593405e7de61345ada248ee
Andrealiz Afable <dreafable@gmail.com>
2016-09-08 20:20:13 -0700

    add filter, filter for employees older than 70

--- app/assets/javascripts/suspicious_filter.js
+++ app/assets/javascripts/suspicious_filter.js
@@ -1,7 +1,15 @@
 angular.module("app").filter('suspicious', [function () {
   return function(employees) {
+    var suspicious = [];
     if (employees) {
-      return [employees[6]];
+      for (var i = 0; i < employees.length; i++) {
+        if (employees[i].birthdate < "1946") {
+          // console.log(employees[i]);
+          suspicious.push(employees[i]);
+          console.log(suspicious);
+        }
+      }
+      return suspicious;
     }
   }
 }]);
