a1eb0795915e15a23232d538d6de108ff773aea5
Andrealiz Afable <dreafable@gmail.com>
2016-09-08 20:56:42 -0700

    fix suspicious filter

--- app/assets/javascripts/suspicious_filter.js
+++ app/assets/javascripts/suspicious_filter.js
@@ -1,15 +1,29 @@
 angular.module("app").filter('suspicious', [function () {
   return function(employees) {
-    var suspicious = [];
+  //   var suspicious = [];
+  //   if (employees) {
+  //     for (var i = 0; i < employees.length; i++) {
+  //       if (employees[i].birthdate < "1946-01-01") {
+  //         // console.log(employees[i]);
+  //         suspicious.push(employees[i]);
+  //       }
+  //     }
+  //     return suspicious;
+  //   }
+  // }
     if (employees) {
+      var suspiciousEmployees = [];
+      var date = new Date();
+      var thisYear = date.getFullYear();
+      var seventyYearsAgo = thisYear - 70;
+
       for (var i = 0; i < employees.length; i++) {
-        if (employees[i].birthdate < "1946") {
-          // console.log(employees[i]);
-          suspicious.push(employees[i]);
-          console.log(suspicious);
+        var birthYear = parseInt($scope.employees[i].birthdate.substring(0, 4));
+        if (birthYear < seventyYearsAgo) {
+          suspiciousEmployees.push(employees[i]);
         }
       }
-      return suspicious;
+      return suspiciousEmployees;
     }
   }
 }]);
